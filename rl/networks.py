4e8a7f24bdc0768bf8850b2fa916b0bb042df620
RamiSketcher <ramisketcher@gmail.com>
2021-09-14 17:24:11 +0000

    Update Networks

--- rl/networks.py
+++ rl/networks.py
@@ -1,16 +1,28 @@
 import random
 import numpy as np
 import torch as T
-import torch.nn as nn
-import torch.nn.functional as F
+from torch.nn.functional import layer_norm
+nn = T.nn
+F = nn.functional
 
 
 
 class MLPNet(nn.Module):
-    def __init__(self, seed):
+    def __init__(self, ip_dim, op_dim, config, seed):
         print('Initialize MLP Network!')
         random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
+
+        layers = []
+        net_arch = config['']
+
+        if net_arch > 0:
+            pass
+
+        if op_dim > 0:
+            pass
+
+        self.net = nn.Sequential(*layers)
         pass
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        return self.net(x)
