ed37f43ba8cce0f3ae5e0058049ce235e296572d
RamiSketcher <ramisketcher@gmail.com>
2021-09-14 17:48:22 +0000

    Update Q-function

--- rl/q_function.py
+++ rl/q_function.py
@@ -1,12 +1,15 @@
 import random
 import numpy as np
 import torch as T
-import torch.nn as nn
+nn = T.nn
 
 from networks import MLPNet
 
 
 class SoftQFunction(nn.Module):
+    """
+    Soft Q-Function
+    """
     def __init__(self, seed):
         print('Initialize QFunction!')
         random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
