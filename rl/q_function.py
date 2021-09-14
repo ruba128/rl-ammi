6d7232213f36a541a79b2d23c6af58136a9929f5
RamiSketcher <ramisketcher@gmail.com>
2021-09-14 16:41:10 +0000

    Update Q-function

--- rl/q_function.py
+++ rl/q_function.py
@@ -1,8 +1,15 @@
+import random
+import numpy as np
+import torch as T
+import torch.nn as nn
 
 
 
-
-class SoftQFunction:
-    def __init__(self):
+class SoftQFunction(nn.Module):
+    def __init__(self, seed):
         print('Initialize QFunction!')
+        random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
+        pass
+
+    def forward(self, o, a):
         pass
