b772c292f84656c862d0d5239330270c2536732b
RamiSketcher <ramisketcher@gmail.com>
2021-09-14 11:11:24 +0000

    Add MFRL

---
+++ rl/mfrl/mfrl.py
@@ -0,0 +1,24 @@
+
+
+from buffer import ReplayBuffer
+
+
+class MFRL:
+    def __init__(self):
+        print('Initialize MFRL!')
+        pass
+
+    def _biuld(slef):
+        pass
+
+    def _set_env(self):
+        pass
+
+    def _set_RB(self):
+        pass
+
+    def internact(self):
+        pass
+
+    def evaluate(self):
+        pass
