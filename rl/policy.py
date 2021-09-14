4b5c79f5a5ddbecaa461091da36311bd3b5aada9
RamiSketcher <ramisketcher@gmail.com>
2021-09-14 16:40:51 +0000

    Update Policy

--- rl/policy.py
+++ rl/policy.py
@@ -1,8 +1,18 @@
+import random
+import numpy as np
+import torch as T
+import torch.nn as nn
 
 
 
+class StochasticPolicy(nn.Module):
+    def __init__(self, seed):
+        print('Initialize Policy!')
+        random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
+        pass
 
-class StochasticPolicy:
-    def __init__(self):
-            print('Initialize Policy!')
-            pass
+    def determinsitic(self):
+        pass
+
+    def forward(self, o):
+        pass
