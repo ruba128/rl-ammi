8612740371378c698697c5b11fe992b37766dfd8
rmutasim@aimsammi.org <rmutasim@aimsammi.org>
2021-09-15 18:48:17 +0000

    Update q_function

--- rl/q_function.py
+++ rl/q_function.py
@@ -10,10 +10,17 @@ class SoftQFunction(nn.Module):
     """
     Soft Q-Function
     """
-    def __init__(self, seed):
+    def __init__(self, obs_dim, act_dim, net_config, seed):
         print('Initialize QFunction!')
         random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
-        pass
 
+        self.critic1 = MLPNet(obs_dim + act_dim, 1, net_config, seed)
+        self.critic2 = MLPNet(obs_dim + act_dim, 1, net_config, seed)
+        
     def forward(self, o, a):
-        pass
+
+        Q_1 = self.critic1(T.cat([o, a], dim=1))
+        Q_2 = self.critic2(T.cat([o, a], dim=1))
+
+        return Q_1, Q_2
+
