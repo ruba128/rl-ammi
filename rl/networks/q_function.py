11af927413a5768750b1bc277086916983622085
RamiSketcher <ramisketcher@gmail.com>
2021-09-18 21:00:30 +0000

    Update q

--- rl/networks/q_function.py
+++ rl/networks/q_function.py
@@ -10,12 +10,12 @@ class SoftQFunction(nn.Module):
     """
     Soft Q-Function
     """
-    def __init__(self, obs_dim, act_dim, net_config, seed):
+    def __init__(self, obs_dim, act_dim, net_configs, seed):
         print('Initialize QFunction!')
         random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
 
-        optimizer = 'nn.optim' + net_configs['critic']['network']['optimizer']
-        lr = self.configs['critic']['network']['lr']
+        optimizer = 'T.optim.' + net_configs['optimizer']
+        lr = net_configs['lr']
 
         self.q1 = MLPNet(obs_dim + act_dim, 1, net_configs, seed)
         self.q2 = MLPNet(obs_dim + act_dim, 1, net_configs, seed)
@@ -23,9 +23,10 @@ class SoftQFunction(nn.Module):
         
         self.optimizer = eval(optimizer)(self.parameters(), lr)
 
+        super().__init__() # To automatically use forward
+
+
     def forward(self, o, a):
-
         q_inputs = T.cat([o, a], dim=-1)
-
         return tuple(Q(q_inputs) for Q in self.Qs)
       
